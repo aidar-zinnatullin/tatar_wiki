fbe875bb21d69d6e7d5a1128a763122dd498c9d9
aidar-zinnatullin <aidar.zinnatulla@gmail.com>
2023-07-26 22:28:14 +0400

    First script

---
+++ TatWiki.R
@@ -0,0 +1,13 @@
+#### Wikipedia
+library(here)
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+library(scales)
+library(grid)
+library(ggpubr)
+options(scipen = 999)
+
+
