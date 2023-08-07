963f280203473f777d08a732f137c3fb7ec24e1e
aidar-zinnatullin <aidar.zinnatulla@gmail.com>
2023-08-07 18:14:33 +0400

    Articles update

--- Analysis.R
+++ Analysis.R
@@ -57,17 +57,184 @@ tat_most_test <- tat_most_test %>% filter(to_remove==FALSE)
 tat_most_test$year <- str_extract_all(pattern = "(\\d{4})", string = tat_most_test$period)
 tat_most_test$year <- unlist(tat_most_test$year)
 
-check_articles_by_year_10 <- tat_most_test %>% group_by(article, year) %>% transmute(n_views = sum(views)) %>% 
+# let's do it from here:
+class(tat_most_test)
+class(tat_most_test$article)
+class(tat_most_test$views)
+class(tat_most_test$year)
+class(tat_most_test$period)
+class(tat_most_test$year)
+table(tat_most_test$year)
+
+check_articles_by_year_10 <- tat_most_test %>% group_by(year, article) %>% transmute(n_views = sum(views)) %>% 
   filter(article!="Баш_бит") 
 
 check_articles_by_year_10 <- check_articles_by_year_10 %>% 
   filter(article!="xss") 
 
+# check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+#   filter(article!="Bötendönya_kileşterelgän_waqıtı") 
+
 check_articles_by_year_10 <- check_articles_by_year_10 %>% 
-  filter(article!="Bötendönya_kileşterelgän_waqıtı") 
+  filter(article!="RSS") 
 
-sorted_articles <- check_articles_by_year_10 %>% group_by(article, year) %>% arrange(desc(n_views)) %>% slice_head(n=10)
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="Wiki") 
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="K")
 
-sorted_articles <- check_articles_by_year_10 %>% group_by(article, year) %>% arrange(desc(n_views)) %>% slice_head(n=10)
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="M")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="Z")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="C")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".ne")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="V")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="H")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="J")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="P")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="W")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="Q")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="X")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="U")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="F")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="Y")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="L")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".au")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="O")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="A")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="N")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="R")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="S")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="B")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="T")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="G")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="D")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="I")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!="E")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".sb")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".cx")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".jp")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".aq")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".mx")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".dj")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".ax")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".lb")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".tr")
+
+check_articles_by_year_10 <- check_articles_by_year_10 %>% 
+  filter(article!=".bb")
+
+
+check_articles_by_year_10$year <- as.factor(check_articles_by_year_10$year)
+class(check_articles_by_year_10$year)
+
+test_for_2023 <- check_articles_by_year_10 %>% filter(year=="2023") %>% slice_head(n=1)
+
+sorted_articles <- check_articles_by_year_10 %>% group_by(year, article) %>% 
+  slice_head(n = 1)%>% arrange(desc(n_views))
+
+sorted_articles_2 <- sorted_articles %>% group_by(year) %>% arrange(desc(year)) %>% slice_head(n=10)
+
+### Now I need to visualize this.
+facet_bar <- function(df, y, x, by, nrow = 2, ncol = 2, scales = "free") {
+  mapping <- aes(y = reorder_within({{ y }}, {{ x }}, {{ by }}), 
+                 x = {{ x }}, 
+                 fill = {{ by }})
+  
+  facet <- facet_wrap(vars({{ by }}), 
+                      nrow = nrow, 
+                      ncol = ncol,
+                      scales = scales) 
+  
+  ggplot(df, mapping = mapping) + 
+    geom_col(show.legend = FALSE) + 
+    scale_y_reordered() + 
+    facet + 
+    ylab("")
+}
+
+sorted_articles_2 %>% 
+  group_by(year) %>% 
+  top_n(10) %>%
+  ungroup() %>%
+  facet_bar(y = article, 
+            x = n_views, 
+            by = year, 
+            nrow = 10)+
+  xlab(label = "Количество просмотров")+
+  theme_minimal()
+ggsave(here("Figures", "Articles.jpeg"), width = 12, height = 9, dpi = 300)
+
+
 
 
